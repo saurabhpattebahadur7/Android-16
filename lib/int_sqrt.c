c51203914ba0601754e293189567f36c950453c4
Rolex040623 <saurabhpattebahadur7@gmail.com>
2026-08-09 06:50:50 +0530

    Refactor int_sqrt and add int_sqrt64 functions

--- lib/int_sqrt.c
+++ lib/int_sqrt.c
@@ -16,14 +16,51 @@
  *
  * A very rough approximation to the sqrt() function.
  */
-unsigned long int_sqrt(unsigned long x)
+inline unsigned long int_sqrt(unsigned long x)
 {
-	unsigned long b, m, y = 0;
+	register unsigned long tmp;
+	register unsigned long place;
+	register unsigned long root = 0;
 
 	if (x <= 1)
 		return x;
 
-	m = 1UL << (__fls(x) & ~1UL);
+	place = 1UL << (BITS_PER_LONG - 2);
+
+	do{
+		place >>= 2;
+	}while(place > x);
+
+	do {
+		tmp = root + place;
+		root >>= 1;
+
+		if (x >= tmp)
+		{
+			x -= tmp;
+			root += place;
+		}
+		place >>= 2;
+	}while (place != 0);
+
+	return root;
+}
+EXPORT_SYMBOL(int_sqrt);
+
+#if BITS_PER_LONG < 64
+/**
+ * int_sqrt64 - strongly typed int_sqrt function when minimum 64 bit input
+ * is expected.
+ * @x: 64bit integer of which to calculate the sqrt
+ */
+u32 int_sqrt64(u64 x)
+{
+	u64 b, m, y = 0;
+
+	if (x <= ULONG_MAX)
+		return int_sqrt((unsigned long) x);
+
+	m = 1ULL << ((fls64(x) - 1) & ~1ULL);
 	while (m != 0) {
 		b = y + m;
 		y >>= 1;
@@ -37,4 +74,4 @@ unsigned long int_sqrt(unsigned long x)
 
 	return y;
 }
-EXPORT_SYMBOL(int_sqrt);
+EXPORT_SYMBOL(int_sqrt64);
